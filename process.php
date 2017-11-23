7bc834b3586b7565d84af25c72b7d29ad8c94f0e
exkungen <23136@ma-web.nl>
2017-11-23 13:01:08 +0100

    dfgdfg

--- process.php
+++ process.php
@@ -1,17 +1,16 @@
 <?php
 
-define('HOST','localhost');
-define('PASS','23136');
-define('USER','exkungen');
-define('DBNAME','23136_database');
 
 
 if(isset($_POST['submit']) && strlen($_POST['beschrijving'] < 220))   {
 
-    $dbc = new PDO('mysql:host=localhost;dbname=23136_database','root', 'root') or die ('Error connecting to database');
-    $stmt = $dbc->prepare("INSERT INTO gastenboek VALUES(0,'$naam','$beschrijving')");
+    $dbc = new PDO('mysql:host=localhost;dbname=23136_database','exkungen', '23136') or die ('Error connecting to database');
+    $stmt = $dbc->prepare("INSERT INTO gastenboek VALUES(0,:naam, :beschrijving)");
     $stmt->bindParam(':naam',$naam);
     $stmt->bindParam(':beschrijving',$beschrijving);
+
+    $naam = $_POST['naam'];
+    $beschrijving = $_POST['beschrijving'];
 
     $beschrijving=htmlspecialchars($_POST['beschrijving']);
     $naam=htmlspecialchars($_POST['naam']);
